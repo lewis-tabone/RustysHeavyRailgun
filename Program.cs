7805370ae301754cb2f1e017ccfd515d4563713f
Lewis <Lewis@DESKTOP-FAOVK91>
2021-10-11 16:40:48 +1000

    Railgun with added timers

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ using SpaceEngineers.Game.ModAPI.Ingame;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using VRage;
@@ -22,62 +21,155 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        // This file contains your actual script.
-        //
-        // You can either keep all your code here, or you can create separate
-        // code files to make your program easier to navigate while coding.
-        //
-        // In order to add a new utility class, right-click on your project, 
-        // select 'New' then 'Add Item...'. Now find the 'Space Engineers'
-        // category under 'Visual C# Items' on the left hand side, and select
-        // 'Utility Class' in the main area. Name it in the box below, and
-        // press OK. This utility class will be merged in with your code when
-        // deploying your final script.
-        //
-        // You can also simply create a new utility class manually, you don't
-        // have to use the template if you don't want to. Just do so the first
-        // time to see what a utility class looks like.
-        // 
-        // Go to:
-        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
-        //
-        // to learn more about ingame scripts.
-
+        SimpleTimerSM timerSM;
+        List<IMyBlockGroup> sabotGroups = new List<IMyBlockGroup>();
+        List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
+        List<IMyBlockGroup> groups = new List<IMyBlockGroup>();
         public Program()
         {
-            // The constructor, called only once every session and
-            // always before any other method is called. Use it to
-            // initialize your script. 
-            //     
-            // The constructor is optional and can be removed if not
-            // needed.
-            // 
-            // It's recommended to set Runtime.UpdateFrequency 
-            // here, which will allow your script to run itself without a 
-            // timer block.
-        }
+            timerSM = new SimpleTimerSM(this, Sequence());
 
-        public void Save()
-        {
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means. 
-            // 
-            // This method is optional and can be removed if not
-            // needed.
+            Echo("Run with 'start' argument");
         }
-
         public void Main(string argument, UpdateType updateSource)
         {
-            // The main entry point of the script, invoked every time
-            // one of the programmable block's Run actions are invoked,
-            // or the script updates itself. The updateSource argument
-            // describes where the update came from. Be aware that the
-            // updateSource is a  bitfield  and might contain more than 
-            // one update type.
-            // 
-            // The method itself is required, but the arguments above
-            // can be removed if not needed.
+            timerSM.Run(); // runs the next step in sequence if started, this can always stay here.
+
+            if (argument == "start")
+            {
+                timerSM.Start();
+
+                // required because timerSM.Run() needs to be called
+                // can also set it to Update100 if the times between waits are long enough or you don't need 100% accuracy.
+                Runtime.UpdateFrequency = UpdateFrequency.Update10;
+            }
+        }
+        IEnumerable<double> Sequence()
+        {
+            GridTerminalSystem.GetBlockGroups(groups);
+            sabotGroups.Clear();
+
+            for (int i = 0; i < groups.Count; i++)
+            {
+                if (groups[i].Name.Contains("Sabot"))
+                    sabotGroups.Add(groups[i]);
+            }
+
+            blocks.Clear();
+            for (int J = 0; J < sabotGroups.Count; J++)
+            {
+                IMyBlockGroup sabot = sabotGroups[J];
+                sabot.GetBlocks(blocks);
+                foreach (var block in blocks)
+                {
+                    var warhead = block as IMyWarhead;
+                    warhead?.StartCountdown();
+                    var functionblock = block as IMyFunctionalBlock;
+                    if (functionblock != null)
+                    {
+                        functionblock.Enabled = !functionblock.Enabled;
+                    }
+                    //block.Enabled = !block.Enabled;                    
+                }
+                yield return 0.2;
+            }
+        }
+        public class SimpleTimerSM
+        {
+            public readonly Program Program;
+
+            /// <summary>
+            /// Wether the timer starts automatically at initialization and auto-restarts it's done iterating.
+            /// </summary>
+            public bool AutoStart { get; set; }
+
+            /// <summary>
+            /// Returns true if a sequence is actively being cycled through.
+            /// False if it ended or no sequence is assigned anymore.
+            /// </summary>
+            public bool Running { get; private set; }
+
+            /// <summary>
+            /// Setting this will change what sequence will be used when it's (re)started.
+            /// </summary>
+            public IEnumerable<double> Sequence;
+
+            /// <summary>
+            /// Time left until the next part is called.
+            /// </summary>
+            public double SequenceTimer { get; private set; }
+
+            private IEnumerator<double> sequenceSM;
+
+            public SimpleTimerSM(Program program, IEnumerable<double> sequence = null, bool autoStart = false)
+            {
+                Program = program;
+                Sequence = sequence;
+                AutoStart = autoStart;
+
+                if (AutoStart)
+                {
+                    Start();
+                }
+            }
+
+            /// <summary>
+            /// (Re)Starts sequence, even if already running.
+            /// Don't forget <see cref="IMyGridProgramRuntimeInfo.UpdateFrequency"/>.
+            /// </summary>
+            public void Start()
+            {
+                SetSequenceSM(Sequence);
+            }
+
+            /// <summary>
+            /// <para>Call this in your <see cref="Program.Main(string, UpdateType)"/> and have a reasonable update frequency, usually Update10 is good for small delays, Update100 for 2s or more delays.</para>
+            /// <para>Checks if enough time passed and executes the next chunk in the sequence.</para>
+            /// <para>Does nothing if no sequence is assigned or it's ended.</para>
+            /// </summary>
+            public void Run()
+            {
+                if (sequenceSM == null)
+                    return;
+
+                SequenceTimer -= Program.Runtime.TimeSinceLastRun.TotalSeconds;
+
+                if (SequenceTimer > 0)
+                    return;
+
+                bool hasValue = sequenceSM.MoveNext();
+
+                if (hasValue)
+                {
+                    SequenceTimer = sequenceSM.Current;
+
+                    if (SequenceTimer <= -0.5)
+                        hasValue = false;
+                }
+
+                if (!hasValue)
+                {
+                    if (AutoStart)
+                        SetSequenceSM(Sequence);
+                    else
+                        SetSequenceSM(null);
+                }
+            }
+
+            private void SetSequenceSM(IEnumerable<double> seq)
+            {
+                Running = false;
+                SequenceTimer = 0;
+
+                sequenceSM?.Dispose();
+                sequenceSM = null;
+
+                if (seq != null)
+                {
+                    Running = true;
+                    sequenceSM = seq.GetEnumerator();
+                }
+            }
         }
     }
 }
